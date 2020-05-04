44acbb3a8e858a73dd2c2cb32659424e6c54ab54
hengyiliu <hengyi@hotmail.com>
2020-05-03 17:26:48 -0700

    rename Models folder to Entities (#7)

--- Controllers/FamiliesController.cs
+++ Controllers/FamiliesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using npmsreg.Helpers;
-using npmsreg.Models;
+using npmsreg.Entities;
 
 namespace npmsreg.Controllers
 {
--- Controllers/PaymentsController.cs
+++ Controllers/PaymentsController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using npmsreg.Models;
+using npmsreg.Entities;
 
 namespace npmsreg.Controllers
 {
